8cafb3ac13826567ccd5cac53426aac4f8363869
Bryan Brophy <brybrophy@gmail.com>
2016-09-10 11:51:37 -0700

    Write server.js file

---
+++ server.js
@@ -0,0 +1,73 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ silent: true });
+}
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// CSRF protection
+app.use('/api', (req, res, next) => {
+  if (/json/.test(req.get('Accept'))) {
+    return next();
+  }
+
+  res.sendStatus(406);
+});
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.use((_req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  if (err.status || (err.output && err.output.statusCode)) {
+    return res
+      .status(err.status || err.output.statusCode)
+      .send(err);
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log(`ON PORT ${port} LISTENING YOU ARE...`);
+  }
+});
+
+module.exports = app;
