fe8709beedd056583834c4a38a3bfa3f36cdc5b3
Bryan Brophy <brybrophy@gmail.com>
2016-09-11 11:14:10 -0700

    Write identities migration test and migration

--- migrations/20160910120816_identities.js
+++ migrations/20160910120816_identities.js
@@ -1,8 +1,21 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-
+exports.up = function(knex) {
+  return knex.schema.createTable('identities', (table) => {
+    table.increments();
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .index();
+    table.string('provider_id').notNullable().defaultTo('');
+    table.string('provider_token').notNullable().defaultTo('');
+    table.string('provider_type').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function(knex) {
   return knex.schema.dropTable('identities');
 };
