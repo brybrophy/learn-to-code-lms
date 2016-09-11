e81a8c3aeb32917acca1ca851a0ae90e46a5ae7f
Bryan Brophy <brybrophy@gmail.com>
2016-09-11 10:56:03 -0700

    Write lessons migration test and migration

--- migrations/20160910120721_users.js
+++ migrations/20160910120721_users.js
@@ -3,7 +3,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments();
-    table.string('name').unique().notNullable().defaultTo('');
+    table.string('name').notNullable().defaultTo('');
     table.string('email').unique().notNullable().defaultTo('');
     table.text('bio').notNullable().defaultTo('');
     table.string('avatar_url').notNullable().defaultTo('');
--- migrations/20160910120748_lessons.js
+++ migrations/20160910120748_lessons.js
@@ -1,8 +1,19 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-
+exports.up = function(knex) {
+  return knex.schema.createTable('lessons', (table) => {
+    table.increments();
+    table.integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .index();
+    table.string('type').notNullable().defaultTo('');
+    table.timestamps(true, true);
+  });
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('lessons');
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
 };
