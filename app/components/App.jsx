e61e27ec6a634a082c55483bbd9fbd541e73547b
Bryan Brophy <brybrophy@gmail.com>
2016-09-12 19:28:01 -0700

    Make landing page fully responisve

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,7 +2,7 @@ import { Tab, Tabs } from 'material-ui/Tabs';
 import AppBar from 'material-ui/AppBar';
 import Menu from 'material-ui/svg-icons/navigation/menu';
 import NavLogo from 'components/NavLogo';
-import Play from 'material-ui/svg-icons/av/play-circle-filled';
+import Play from 'material-ui/svg-icons/av/play-arrow';
 import React from 'react';
 // import SwipeableViews from 'react-swipeable-views';
 
@@ -20,7 +20,7 @@ const App = React.createClass({
 
         <Tabs
           inkBarStyle={{ marginLeft: '7%', width: '5%' }}
-          className="tabs"
+          className="tabs hideSm"
           tabItemContainerStyle={{ backgroundColor: 'none' }}
         >
           <Tab
@@ -54,20 +54,37 @@ const App = React.createClass({
       </nav>
       <main className="main">
         <section className="mainContent">
-          <h1 className="mainHeader">Learn</h1>
-          <h1 className="mainHeader">To Code</h1>
-          <p className="mainSubHeader">Welcome to ltc</p>
-          <p className="mainText">A complete set of tools to learn</p>
-          <p className="mainText">the basics of web development</p>
+          <img className="hideLg bracket" src="/images/bracket-left.png"></img>
+          <div className="inlineSm">
+            <h1 className="mainHeader">Learn</h1>
+            <h1 className="mainHeader">To Code</h1>
+            <p className="mainSubHeader">Welcome to ltc</p>
+            <p className="mainText">A complete set of tools to learn</p>
+            <p className="mainText">the basics of web development</p>
+          </div>
+          <img className="hideLg bracket" src="/images/bracket-right.png"></img>
         </section>
 
-        <section className="mainContent">
+        <section className="mainContent heroLogoLg">
           <img src="/images/hero-logo.png"></img>
         </section>
 
-        <section className="mainContent">
+        <section className="mainContent hideSm">
           <div className="playContainer">
-            <Play className="play" />
+            <div className="play">
+              <Play style={{ color: '#FFFFFF' }} />
+            </div>
+            <div style={{ flex: '1' }}>
+              <p className="playLabel">Tour <br />Curriculum</p>
+            </div>
+          </div>
+        </section>
+
+        <section className="mainContent hideLg">
+          <div className="playContainer">
+            <div className="play">
+              <Play style={{ color: '#FFFFFF'}} />
+            </div>
             <div style={{ flex: '1' }}>
               <p className="playLabel">Tour <br />Curriculum</p>
             </div>
