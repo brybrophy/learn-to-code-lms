2f92e5adee6cb7c8e8ae8870ceca4504ba00a4d7
Bryan Brophy <brybrophy@gmail.com>
2016-10-23 23:48:48 -0700

    Hide button when logged in

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -20,6 +20,20 @@ const About = React.createClass({
   },
 
   render() {
+    let GetStarted;
+
+    if (cookie.load('loggedIn')) {
+      GetStarted = (
+        <a
+          className="getStartedBtn"
+          href="#"
+          onTouchTap={this.handleTouchTap}
+        >
+          GET STARTED
+        </a>
+      );
+    }
+
     return <main className="mainAbout">
       <section className="mainContent">
         <h1 className="mainHeader aboutHeader">Learn To Code</h1>
@@ -32,13 +46,7 @@ const About = React.createClass({
             {' '}Galvanize
           </a>
           , a school and co-working space that is dedicated to transforming lives and building community. Galvanize offers immersive programs in Web Development and Data Science that have helped hundreds of people level up their careers in a matter of months. Learn to code classes are a great first step in the journey to becoming a developer and we hope that the foundation they provide will help you as your journey continues. Good Luck!</p>
-        <a
-          className="getStartedBtn"
-          href="#"
-          onTouchTap={this.handleTouchTap}
-        >
-          GET STARTED
-        </a>
+        {GetStarted}
       </section>
       <section className="mainContent events">
         <h3 className="eventsHeader">Upcoming Classes</h3>
